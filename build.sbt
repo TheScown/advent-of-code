5be3a6151fd083b5325a22471839bbd67e3cccf2
Alex Scown <alex@scown.space>
2023-12-03 12:41:46 +0000

    Day 3 solution
    Bring in the parser combinators

--- build.sbt
+++ build.sbt
@@ -2,6 +2,8 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.10"
 
+libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "2.2.0"
+
 lazy val root = (project in file("."))
   .settings(
     name := "advent-of-code-2023",
--- src/main/scala/lib/Files.scala
+++ src/main/scala/lib/Files.scala
@@ -2,10 +2,14 @@ package space.scown.advent2023
 package lib
 
 import scala.io.Source
+import scala.util.parsing.input.PagedSeq
 
 object Files {
   def lines(filename: String): Vector[String] = {
     Source.fromResource(filename).getLines().toVector
   }
 
+  def pagedSequence(filename: String): PagedSeq[Char] = {
+    PagedSeq.fromSource(Source.fromResource(filename))
+  }
 }
