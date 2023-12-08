4aff35d839df2ad5d8afa489d07c0c77437d6760
Alex Scown <alex@scown.space>
2023-12-08 22:25:12 +0000

    Day 5: Position modes

--- src/main/scala/intcode/IntcodeComputer.scala
+++ src/main/scala/intcode/IntcodeComputer.scala
@@ -8,14 +8,14 @@ case class IntcodeComputer(program: Vector[Int], input: () => Int = () => 42, ou
   def execute(): Vector[Int] = {
     @tailrec
     def helper(pc: Int, memory: Vector[Int]): Vector[Int] = {
-      println(memory)
+      println(s"$pc, $memory")
 
-      memory(pc) match {
-        case 1 => helper(pc + 4, add(pc, memory))
-        case 2 => helper(pc + 4, multiply(pc, memory))
-        case 3 => helper(pc + 1, read(pc, memory))
-        case 4 => helper(pc + 1, write(pc, memory))
-        case 99 => memory
+      Instruction.fromInt(memory(pc)) match {
+        case Instruction(1, positionModes) => helper(pc + 4, add(pc, memory, positionModes))
+        case Instruction(2, positionModes) => helper(pc + 4, multiply(pc, memory, positionModes))
+        case Instruction(3, positionModes) => helper(pc + 2, read(pc, memory, positionModes))
+        case Instruction(4, positionModes) => helper(pc + 2, write(pc, memory, positionModes))
+        case Instruction(99, _) => memory
         case x => throw new IllegalStateException(s"Illegal opcode $x at $pc. State: $memory")
       }
     }
@@ -23,20 +23,73 @@ case class IntcodeComputer(program: Vector[Int], input: () => Int = () => 42, ou
     helper(0, program)
   }
 
-  private def add(pc: Int, memory: Vector[Int]) = {
-    memory.updated(memory(pc + 3), memory(memory(pc + 1)) + memory(memory(pc + 2)))
+  private def add(pc: Int, memory: Vector[Int], positionModes: Vector[PositionMode]) = {
+    memory.updated(positionModes(2).dest(pc, memory), positionModes(0)(pc, memory) + positionModes(1)(pc, memory))
   }
 
-  private def multiply(pc: Int, memory: Vector[Int]) = {
-    memory.updated(memory(pc + 3), memory(memory(pc + 1)) * memory(memory(pc + 2)))
+  private def multiply(pc: Int, memory: Vector[Int], positionModes: Vector[PositionMode]) = {
+    memory.updated(positionModes(2).dest(pc, memory), positionModes(0)(pc, memory) * positionModes(1)(pc, memory))
   }
 
-  private def read(pc: Int, memory: Vector[Int]) = {
-    memory.updated(memory(pc + 1), input())
+  private def read(pc: Int, memory: Vector[Int], positionModes: Vector[PositionMode]) = {
+    memory.updated(positionModes(0).dest(pc, memory), input())
   }
 
-  private def write(pc: Int, memory: Vector[Int]) = {
-    output(memory(pc + 1))
+  private def write(pc: Int, memory: Vector[Int], positionModes: Vector[PositionMode]) = {
+    output(positionModes(0)(pc, memory))
     memory
   }
+}
+
+case class Instruction(opcode: Int, positionModes: Vector[PositionMode])
+
+object Instruction {
+
+  def fromInt(rawOpcode: Int): Instruction = {
+    val opcode = rawOpcode % 100
+    val positionModeInt = (rawOpcode - opcode) / 100
+    val positionModes = (1 to 3).map { m =>
+      PositionMode.fromInt((positionModeInt / Math.pow(10, m - 1).toInt) % Math.pow(10, m).toInt, m)
+    }.toVector
+
+    Instruction(opcode, positionModes)
+  }
+
+}
+
+sealed trait PositionMode {
+  def apply(pc: Int, memory: Vector[Int]): Int
+
+  def dest(pc: Int, memory: Vector[Int]): Int
+}
+
+case class Position(offset: Int) extends PositionMode {
+  override def apply(pc: Int, memory: Vector[Int]): Int = {
+    val pointer = memory(pc + offset)
+    memory(pointer)
+  }
+
+  override def dest(pc: Int, memory: Vector[Int]): Int = {
+    memory(pc + offset)
+  }
+}
+
+case class Immediate(offset: Int) extends PositionMode {
+  override def apply(pc: Int, memory: Vector[Int]): Int = {
+    memory(pc + offset)
+  }
+
+  override def dest(pc: Int, memory: Vector[Int]): Int = {
+    pc + offset
+  }
+}
+
+object PositionMode {
+
+  def fromInt(p: Int, offset: Int): PositionMode = p match {
+    case 0 => Position(offset)
+    case 1 => Immediate(offset)
+    case _ => throw new IllegalStateException(s"Invalid position mode $p")
+  }
+
 }
