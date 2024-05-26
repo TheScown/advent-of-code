c77350c86022579b76142d03c1a2e0a2591ed6bf
Alex Scown <alex@scown.space>
2024-05-26 10:18:27 +0100

    2015 Day 10 optimised
    Implement the approach described on the
    Wikipedia page for the sequence to avoid
    the String munging entirely, making it
    much faster.

--- src/main/scala/advent2015/problems/Day10.scala
+++ src/main/scala/advent2015/problems/Day10.scala
@@ -1,52 +1,59 @@
 package space.scown.adventofcode
 package advent2015.problems
 
+import advent2015.problems.Day10.{Element, elementsByName, elementsBySequence}
 import lib.{Files, Problem, Timer}
 
 import scala.annotation.tailrec
 
 case class Day10(input: String) extends Problem {
   override def solve1(): Unit = {
-    @tailrec
-    def helper(input: String, remainingIterations: Int): String = {
-      if (remainingIterations == 0) input
-      else {
-        println(remainingIterations)
-        helper(processString(new StringBuilder(input)), remainingIterations - 1)
-      }
-    }
+    val startElement = elementsBySequence(input)
 
-    val result = helper(input, 40).length
+    val result = helper(Seq(startElement), 40).map(_.sequence.length).sum
     println(s"Result 1: $result")
   }
 
   override def solve2(): Unit = {
-    @tailrec
-    def helper(input: String, remainingIterations: Int): String = {
-      if (remainingIterations == 0) input
-      else {
-        println(remainingIterations)
-        helper(processString(new StringBuilder(input)), remainingIterations - 1)
-      }
-    }
+    val startElement = elementsBySequence(input)
 
-    val result = helper(input, 50).length
+    val result = helper(Seq(startElement), 50).map(_.sequence.length).sum
     println(s"Result 2: $result")
   }
 
   @tailrec
-  private def processString(input: StringBuilder, output: StringBuilder = new StringBuilder()): String = {
-    if (input.isEmpty) output.toString()
+  private def helper(input: Seq[Day10.Element], remainingIterations: Int): Seq[Element] = {
+    if (remainingIterations == 0) input
     else {
-      val prefix = input.takeWhile(c => c == input.head)
-      processString(input.delete(0, prefix.length), output.append(s"${prefix.length}${prefix.charAt(0)}"))
+      val nextElements = input.flatMap {
+        e => e.decaysInto.map(id => elementsByName(id))
+      }
+
+      helper(nextElements, remainingIterations - 1)
     }
   }
 }
 
-case object Day10 extends App {
-  val input = Files.lines("2015/day10.txt")(0)
-  val problem = Day10(input)
-  Timer.time(() => problem.solve1())
-  Timer.time(() => problem.solve2())
+case object Day10 {
+
+  private val elements = parseData
+  private val elementsByName = elements.map(e => e.name -> e).toMap
+  val elementsBySequence: Map[String, Element] = elements.map(e => e.sequence -> e).toMap
+
+  def main(args: Array[String]): Unit = {
+    val input = Files.lines("2015/day10.txt")(0)
+    val problem = Day10(input)
+    Timer.time(() => problem.solve1())
+    Timer.time(() => problem.solve2())
+  }
+
+  private def parseData = {
+    val pattern = "\\d+\t([A-Za-z]+)\t(\\d+)\t([A-Za-z]+(?:.[A-Za-z]+)*).*".r
+    Files.lines("2015/look-and-say-elements.tsv").map {
+      case pattern(element, sequence, decayPattern) =>
+        Element(element, sequence, decayPattern.split("\\."))
+    }
+  }
+
+  case class Element(name: String, sequence: String, decaysInto: Seq[String])
 }
