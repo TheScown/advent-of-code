b305c464ab0d6104e65ee0c86fb3e89ecb04bcb8
Alex Scown <alex@scown.space>
2024-06-27 19:58:05 +0100

    2016 Day 5 with animation

--- src/main/scala/advent2016/Day5.scala
+++ src/main/scala/advent2016/Day5.scala
@@ -4,6 +4,7 @@ package advent2016
 import lib.{Crypto, Files, Integers, Problem, Timer}
 
 import scala.annotation.tailrec
+import scala.util.Random
 
 case class Day5(input: String) extends Problem {
   override def solve1(): Unit = {
@@ -21,7 +22,6 @@ case class Day5(input: String) extends Problem {
   override def solve2(): Unit = {
     val characters = Integers.naturalNumbers
       .map(n => Crypto.md5(input + n))
-      .filter(s => s.startsWith("00000"))
 
     @tailrec
     def helper(state: State): Map[Int, Char] = {
@@ -32,26 +32,31 @@ case class Day5(input: String) extends Problem {
     val finalMap = helper(State(characters))
     val result = (0 until 8).map(finalMap).mkString("")
 
+    println()
     println(s"Result 2: $result")
   }
 
   case class State(lazyList: LazyList[String], found: Map[Int, Char] = Map()) {
     private val range = 0 until 8
+    private val unknown = "!@£$%^&*#"
+    private val random = Random
     def isFinished: Boolean = range.forall(found.contains)
 
     def next: State = {
       @tailrec
       def helper(lazyList: LazyList[String]): ((Int, Char), LazyList[String]) = {
+        val nextResultString = (0 until 8).map(i => found.getOrElse(i, unknown.charAt(random.nextInt(unknown.length)))).mkString("")
+
+        print(s"\r$nextResultString")
+
         val nextString = lazyList.head
         val c = nextString.charAt(5).asDigit
 
-        if (range.contains(c) && !found.contains(c)) ((c, nextString.charAt(6)), lazyList.tail)
+        if (nextString.startsWith("00000") && range.contains(c) && !found.contains(c)) ((c, nextString.charAt(6)), lazyList.tail)
         else helper(lazyList.tail)
       }
 
       val (nextResult, newList) = helper(lazyList)
-
-      println(s"Found $nextResult")
 
       State(newList, found + nextResult)
     }
@@ -61,6 +66,6 @@ case class Day5(input: String) extends Problem {
 case object Day5 extends App {
   val input = Files.lines("2016/day5.txt").head
   val problem = Day5(input)
-//  Timer.time(() => problem.solve1())
+  Timer.time(() => problem.solve1())
   Timer.time(() => problem.solve2())
 }
