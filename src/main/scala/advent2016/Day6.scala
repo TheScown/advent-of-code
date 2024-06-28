adc2ffa3619f82eb1065e94b7df9884034c6d879
Alex Scown <alex@scown.space>
2024-06-28 17:47:57 +0100

    2016 Day 6

---
+++ src/main/scala/advent2016/Day6.scala
@@ -0,0 +1,36 @@
+package space.scown.adventofcode
+package advent2016
+
+import lib.{Files, Problem}
+
+case class Day6(input: Vector[String]) extends Problem {
+  override def solve1(): Unit = {
+    val result = solve(v => v.maxBy(_._2))
+
+    println(s"Result 1: $result")
+  }
+
+  override def solve2(): Unit = {
+    val result = solve(v => v.minBy(_._2))
+
+    println(s"Result 2: $result")
+  }
+
+  private def solve(getValue: Vector[(Char, Int)] => (Char, Int)) = {
+    val transpose = input.map(_.toVector).transpose
+    val result = transpose.map { col =>
+      getValue(col.groupBy(identity)
+        .view.mapValues(_.size)
+        .toVector)
+        ._1
+    }.mkString("")
+    result
+  }
+}
+
+case object Day6 extends App {
+  val input = Files.lines("2016/day6.txt")
+  val problem = Day6(input)
+  problem.solve1()
+  problem.solve2()
+}
