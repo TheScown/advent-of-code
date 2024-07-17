a3878083e659f95cc94c17aa51e708eebd01a1fd
Alex Scown <alex@scown.space>
2024-07-17 17:43:01 +0100

    2016 Day 11
    Part 2 takes 3 minutes, so further optimisation is possible

---
+++ src/main/scala/advent2016/problems/Day11.scala
@@ -0,0 +1,137 @@
+package space.scown.adventofcode
+package advent2016.problems
+
+import lib.{Files, Problem, Timer}
+
+import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+
+case class Day11(input: Vector[String]) extends Problem {
+  override def solve1(): Unit = {
+    val floors = parse()
+
+    val result = solve(floors)
+
+    println(s"Result 1: $result")
+  }
+
+  override def solve2(): Unit = {
+    val floors = parse()
+    val updatedFloors = floors.updated(0, Floor(floors(0).things ++ Set(
+      Thing("elerium", "generator"),
+      Thing("elerium", "microchip"),
+      Thing("dilithium", "generator"),
+      Thing("dilithium", "microchip")
+    )))
+
+    val result = solve(updatedFloors)
+
+    println(s"Result 2: $result")
+  }
+
+  private def solve(floors: Vector[Floor]): Int = {
+    @tailrec
+    def helper(queue: Queue[State], seen: Set[State]): Int = {
+      if (queue.isEmpty) throw new IllegalStateException("No more states to test")
+      else {
+        val (next, nextQueue) = queue.dequeue
+        next match {
+          case State(elevatorFloor, floors, moves) =>
+            val minFloor = floors.indexWhere(f => f.things.nonEmpty)
+            val nextFloors = Seq(elevatorFloor - 1, elevatorFloor + 1).filter(f => f >= minFloor && f < floors.size)
+            val newStates = nextFloors.flatMap { nextFloorId =>
+              val currentFloor = floors(elevatorFloor)
+              val nextFloor = floors(nextFloorId)
+              val currentFloorThings = currentFloor.things.toVector
+
+              val possibleMoves = currentFloorThings.combinations(2).toVector ++ currentFloorThings.combinations(1)
+              possibleMoves.map { move =>
+                val newCurrentFloor = currentFloor.copy(things = (currentFloorThings diff move).toSet)
+                val newNextFloor = nextFloor.copy(things = nextFloor.things ++ move)
+                State(
+                  nextFloorId,
+                  floors
+                    .updated(elevatorFloor, newCurrentFloor)
+                    .updated(nextFloorId, newNextFloor),
+                  moves + 1
+                )
+              }
+                .filterNot(state => seen.contains(state))
+                .filter(state => state.floors.forall(_.isValid))
+            }
+
+            newStates.find(_.isComplete) match {
+              case Some(State(_, _, moves)) => moves
+              case None =>
+                val updatedQueue = newStates.foldLeft(nextQueue)((queue, state) => queue.enqueue(state))
+                helper(updatedQueue, seen ++ newStates)
+            }
+        }
+      }
+    }
+
+    val initialState = State(0, floors, 0)
+    val result = helper(Queue(initialState), Set(initialState))
+    result
+  }
+
+  private def parse(): Vector[Floor] = {
+    val prefix = "^The [a-z]+ floor contains ".r
+    input.map { line =>
+      val preProcessed = prefix.replaceFirstIn(line, "").replace(".", "")
+      preProcessed match {
+        case "nothing relevant" => Floor()
+        case _ =>
+          val things = preProcessed.split(", (?:and )?")
+            .map(_.replace("a ", ""))
+            .map { s =>
+              val parts = s.split(" ")
+              val purpose = parts(1)
+              val element = parts(0).replace("-compatible", "")
+              Thing(element, purpose)
+            }
+            .toSet
+
+          Floor(things)
+      }
+    }
+  }
+
+  case class State(elevatorFloor: Int, floors: Vector[Floor], moves: Int) {
+    def isComplete: Boolean = {
+      val reversed = floors.reverse
+      reversed.tail.forall(f => f.things.isEmpty)
+    }
+
+    override def equals(obj: Any): Boolean = {
+      if (!obj.isInstanceOf[State]) false
+      else {
+        val other = obj.asInstanceOf[State]
+
+        if (other.elevatorFloor != elevatorFloor) false
+        else other.floors == floors
+      }
+    }
+
+    override def hashCode(): Int = 37 * elevatorFloor + floors.hashCode()
+  }
+
+  case class Floor(things: Set[Thing] = Set()) {
+    def isValid: Boolean = {
+      val generators = things.filter(_.purpose == "generator")
+
+      if (generators.isEmpty) true
+      else things.filter(_.purpose == "microchip")
+        .forall(t => things.exists(s => s.element == t.element && s.purpose == "generator"))
+    }
+  }
+
+  case class Thing(element: String, purpose: String)
+}
+
+case object Day11 extends App {
+  val input = Files.lines("2016/day11.txt")
+  val problem = Day11(input)
+  Timer.time(() => problem.solve1())
+  Timer.time(() => problem.solve2())
+}
