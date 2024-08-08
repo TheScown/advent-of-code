1bf2d6b3e1a41807233b53ebe337d1c4861230e6
Alex Scown <alex@scown.space>
2024-08-08 10:52:57 +0100

    2017 Day 12
    Build a UnionFind using path splitting and union by size

---
+++ src/main/scala/advent2017/problems/Day12.scala
@@ -0,0 +1,49 @@
+package space.scown.adventofcode
+package advent2017.problems
+
+import lib.{Files, Problem, UnionFind}
+
+case class Day12(input: Vector[String]) extends Problem {
+  override def solve1(): Unit = {
+    val instructions = parse()
+    val unionFind: UnionFind = connectComponents(instructions)
+
+    val result = unionFind.size(0)
+
+    println(s"Result 1: $result")
+  }
+
+  override def solve2(): Unit = {
+    val instructions = parse()
+    val unionFind: UnionFind = connectComponents(instructions)
+
+    val result = instructions.indices.map(unionFind.find).toSet.size
+
+    println(s"Result 2: $result")
+  }
+
+  private def connectComponents(instructions: Vector[(Int, Set[Int])]) = {
+    val unionFind = UnionFind(instructions.size)
+
+    instructions.foreach {
+      case (head, tail) => tail.foreach(c => unionFind.union(head, c))
+    }
+    unionFind
+  }
+
+  def parse(): Vector[(Int, Set[Int])] = {
+    input.map { line =>
+      val parts = line.split("<->")
+      val head  = parts(0).trim.toInt
+      val tail = parts(1).split(",").map(_.trim.toInt).toSet
+      (head, tail)
+    }
+  }
+}
+
+case object Day12 extends App {
+  val input = Files.lines("2017/day12.txt")
+  val problem = Day12(input)
+  problem.solve1()
+  problem.solve2()
+}
---
+++ src/main/scala/lib/UnionFind.scala
@@ -0,0 +1,46 @@
+package space.scown.adventofcode
+package lib
+
+import scala.annotation.tailrec
+import scala.collection.mutable
+
+case class UnionFind(size: Int) {
+
+  private val parents: mutable.IndexedSeq[Int] = mutable.IndexedSeq.range(0, size)
+  private val sizes: mutable.IndexedSeq[Int] = mutable.IndexedSeq.fill(size)(1)
+
+  def find(x: Int): Int = {
+    @tailrec
+    def helper(node: Int): Int = {
+      val parent = parents(node)
+
+      if (parent == node) node
+      else {
+        val grandparent = parents(parents(x))
+        parents(x) = grandparent
+        helper(parent)
+      }
+    }
+
+    helper(x)
+  }
+
+  def union(x: Int, y: Int): Unit = {
+    val xRoot = find(x)
+    val yRoot = find(y)
+
+    if (xRoot != yRoot) {
+      val (smallRoot, largeRoot) = if (sizes(xRoot) < sizes(yRoot)) (xRoot, yRoot) else (yRoot, xRoot)
+
+      parents(smallRoot) = largeRoot
+      sizes(largeRoot) = sizes(largeRoot) + sizes(smallRoot)
+    }
+  }
+
+  def size(x: Int): Int = sizes(find(x))
+
+}
+
+case object UnionFind {
+
+}
