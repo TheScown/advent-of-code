502d6353ed183d80c16e39682dbdd49b7079879c
Alex Scown <alex@scown.space>
2024-08-11 19:28:50 +0100

    2017 Day 19

---
+++ src/main/scala/advent2017/problems/Day19.scala
@@ -0,0 +1,78 @@
+package space.scown.adventofcode
+package advent2017.problems
+
+import lib.{Complex, Files, Grid, Problem}
+
+import scala.annotation.tailrec
+import scala.math.Integral.Implicits.infixIntegralOps
+
+case class Day19(input: Vector[String]) extends Problem {
+  override def solve1(): Unit = {
+    val grid = buildGrid()
+    val start = grid.indexOf('|').get
+
+    val pathCharacters = Set('-', '|')
+
+    @tailrec
+    def helper(position: Complex, direction: Complex, acc: Vector[Char]): Vector[Char] = {
+      if (grid(position) == '+') {
+        val nextPosition = grid.neighbours(position)
+          .filterNot(grid(_) == ' ')
+          .filterNot(_ == position - direction)
+          .head
+        val nextDirection = nextPosition - position
+
+        helper(nextPosition, nextDirection, acc)
+      }
+      else if (grid(position) == ' ') acc
+      else if (pathCharacters.contains(grid(position))) helper(position + direction, direction, acc)
+      else helper(position + direction, direction, acc :+ grid(position))
+    }
+
+    val result = helper(start, -Complex.I, Vector()).mkString("")
+
+    println(s"Result 1: $result")
+  }
+
+  override def solve2(): Unit = {
+    val grid = buildGrid()
+    val start = grid.indexOf('|').get
+
+    @tailrec
+    def helper(position: Complex, direction: Complex, acc: Int): Int = {
+      if (grid(position) == '+') {
+        val nextPosition = grid.neighbours(position)
+          .filterNot(grid(_) == ' ')
+          .filterNot(_ == position - direction)
+          .head
+        val nextDirection = nextPosition - position
+
+        helper(nextPosition, nextDirection, acc + 1)
+      }
+      else if (grid(position) == ' ') acc - 1
+      else helper(position + direction, direction, acc + 1)
+    }
+
+    val result = helper(start, -Complex.I, 1)
+
+    println(s"Result 2: $result")
+  }
+
+  private def buildGrid(): Grid[Char] = {
+    val maxLineLength = input.maxBy(_.length).length
+
+    val paddedLines = input.map { line =>
+      if (line.length == maxLineLength) line
+      else line + (" " * (maxLineLength - line.length))
+    }
+
+    Grid(paddedLines.map(_.toVector))
+  }
+}
+
+case object Day19 extends App {
+  val input = Files.lines("2017/day19.txt")
+  val problem = Day19(input)
+  problem.solve1()
+  problem.solve2()
+}
