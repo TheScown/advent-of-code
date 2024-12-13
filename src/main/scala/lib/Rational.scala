b97acb8a1ce3250e3d740fc48dac22b51bd47105
Alex Scown <alex@scown.space>
2024-12-13 12:31:53 +0000

    Fix Rational

--- src/main/scala/lib/Rational.scala
+++ src/main/scala/lib/Rational.scala
@@ -2,69 +2,64 @@ package space.scown.adventofcode
 package lib
 
 import scala.language.implicitConversions
-import scala.math.Numeric.{BigIntIsIntegral, IntIsIntegral, LongIsIntegral}
 import scala.math.{ScalaNumber, ScalaNumericConversions}
 
-case class Rational[T](d: T, q: T)(implicit n: Integral[T])  extends ScalaNumber with ScalaNumericConversions {
+case class Rational[T](d: T, q: T)(implicit n: Integral[T]) extends ScalaNumber with ScalaNumericConversions {
 
   if (q == n.zero) {
     throw new ArithmeticException("/ by zero")
   }
 
   def simplify: Rational[T] = {
-    val (factor, _, _) = Integers.extendedEuclidean(n.abs(d), n.abs(q))
+    val factor = Integers.gcd(d, q)
     if (factor == n.one) this
-    else Rational(n.quot(d, factor), n.quot(q, factor))(n).simplify
+    else Rational(n.quot(d, factor), n.quot(q, factor)).simplify
   }
-
-  def *(x: T): Rational[T] = Rational(n.times(x, d), q).simplify
 
   override def underlying(): AnyRef = this
 
   override def isWhole: Boolean = q == n.one
 
-  override def intValue: Int = ???
+  override def intValue: Int = n.toInt(d) / n.toInt(q)
 
-  override def longValue: Long = ???
+  override def longValue: Long = n.toLong(d) / n.toLong(q)
 
-  override def floatValue: Float = ???
+  override def floatValue: Float = n.toFloat(d) / n.toFloat(q)
 
-  override def doubleValue: Double = ???
+  override def doubleValue: Double = n.toDouble(d) / n.toDouble(q)
 }
-
 case object Rational {
   def ZERO[T](implicit n: Integral[T]): Rational[T] = Rational(n.zero, n.one)
 
   trait RationalIntIsFractional extends Fractional[Rational[Int]] {
-    private def n = IntIsIntegral
 
     override def plus(x: Rational[Int], y: Rational[Int]): Rational[Int] = {
-      if (x.q == y.q) Rational(x.d + y.d, x.q)(n).simplify
+      if (x.q == y.q) Rational(x.d + y.d, x.q).simplify
       else {
-        Rational(x.d * y.q + y.d * x.q, x.q * y.q)(n).simplify
+        Rational(x.d * y.q + y.d * x.q, x.q * y.q).simplify
       }
     }
 
     override def minus(x: Rational[Int], y: Rational[Int]): Rational[Int] = {
-      if (x.q == y.q) Rational(x.d - y.d, x.q)(n).simplify
+      if (x.q == y.q) Rational(x.d - y.d, x.q).simplify
       else {
-        Rational(x.d * y.q - y.d * x.q, x.q * y.q)(n).simplify
+        Rational(x.d * y.q - y.d * x.q, x.q * y.q).simplify
       }
     }
 
-    override def times(x: Rational[Int], y: Rational[Int]): Rational[Int] = Rational(x.d * y.d, x.q * y.q)(n).simplify
+    override def times(x: Rational[Int], y: Rational[Int]): Rational[Int] = Rational(x.d * y.d, x.q * y.q).simplify
 
     override def div(x: Rational[Int], y: Rational[Int]): Rational[Int] = {
       if (y.d == 0) throw new ArithmeticException("/ by zero")
-      else Rational(x.d * y.q, x.q * y.d)(n).simplify
+      else Rational(x.d * y.q, x.q * y.d).simplify
     }
-    override def negate(x: Rational[Int]): Rational[Int] = Rational(-x.d, x.q)(n).simplify
+    override def negate(x: Rational[Int]): Rational[Int] = Rational(-x.d, x.q).simplify
 
     override def fromInt(x: Int): Rational[Int] = Rational(x, 1)
 
     override def parseString(str: String): Option[Rational[Int]] = ???
 
-    override def toInt(x: Rational[Int]): Int = x.d.toInt / x.q.toInt
+    override def toInt(x: Rational[Int]): Int = x.d / x.q
 
     override def toLong(x: Rational[Int]): Long = x.d.toLong / x.q.toLong
 
@@ -81,29 +76,28 @@ case object Rational {
   implicit object RationalIntIsFractional extends RationalIntIsFractional with Ordering[Rational[Int]]
 
   trait RationalLongIsFractional extends Fractional[Rational[Long]] {
-    private def n = LongIsIntegral
 
     override def plus(x: Rational[Long], y: Rational[Long]): Rational[Long] = {
-      if (x.q == y.q) Rational(x.d + y.d, x.q)(n).simplify
+      if (x.q == y.q) Rational(x.d + y.d, x.q).simplify
       else {
-        Rational(x.d * y.q + y.d * x.q, x.q * y.q)(n).simplify
+        Rational(x.d * y.q + y.d * x.q, x.q * y.q).simplify
       }
     }
 
     override def minus(x: Rational[Long], y: Rational[Long]): Rational[Long] = {
-      if (x.q == y.q) Rational(x.d - y.d, x.q)(n).simplify
+      if (x.q == y.q) Rational(x.d - y.d, x.q).simplify
       else {
-        Rational(x.d * y.q - y.d * x.q, x.q * y.q)(n).simplify
+        Rational(x.d * y.q - y.d * x.q, x.q * y.q).simplify
       }
     }
 
-    override def times(x: Rational[Long], y: Rational[Long]): Rational[Long] = Rational(x.d * y.d, x.q * y.q)(n).simplify
+    override def times(x: Rational[Long], y: Rational[Long]): Rational[Long] = Rational(x.d * y.d, x.q * y.q).simplify
 
     override def div(x: Rational[Long], y: Rational[Long]): Rational[Long] = {
       if (y.d == 0) throw new ArithmeticException("/ by zero")
-      else Rational(x.d * y.q, x.q * y.d)(n).simplify
+      else Rational(x.d * y.q, x.q * y.d).simplify
     }
-    override def negate(x: Rational[Long]): Rational[Long] = Rational(-x.d, x.q)(n).simplify
+    override def negate(x: Rational[Long]): Rational[Long] = Rational(-x.d, x.q).simplify
 
     override def fromInt(x: Int): Rational[Long] = Rational(x, 1)
 
@@ -111,7 +105,7 @@ case object Rational {
 
     override def toInt(x: Rational[Long]): Int = x.d.toInt / x.q.toInt
 
-    override def toLong(x: Rational[Long]): Long = x.d.toLong / x.q.toLong
+    override def toLong(x: Rational[Long]): Long = x.d / x.q
 
     override def toFloat(x: Rational[Long]): Float = x.d.toFloat / x.q.toFloat
 
@@ -126,29 +120,28 @@ case object Rational {
   implicit object RationalLongIsFractional extends RationalLongIsFractional with Ordering[Rational[Long]]
 
   trait RationalBigIntIsFractional extends Fractional[Rational[BigInt]] {
-    private def n = BigIntIsIntegral
 
     override def plus(x: Rational[BigInt], y: Rational[BigInt]): Rational[BigInt] = {
-      if (x.q == y.q) Rational(x.d + y.d, x.q)(n).simplify
+      if (x.q == y.q) Rational(x.d + y.d, x.q).simplify
       else {
-        Rational(x.d * y.q + y.d * x.q, x.q * y.q)(n).simplify
+        Rational(x.d * y.q + y.d * x.q, x.q * y.q).simplify
       }
     }
 
     override def minus(x: Rational[BigInt], y: Rational[BigInt]): Rational[BigInt] = {
-      if (x.q == y.q) Rational(x.d - y.d, x.q)(n).simplify
+      if (x.q == y.q) Rational(x.d - y.d, x.q).simplify
       else {
-        Rational(x.d * y.q - y.d * x.q, x.q * y.q)(n).simplify
+        Rational(x.d * y.q - y.d * x.q, x.q * y.q).simplify
       }
     }
 
-    override def times(x: Rational[BigInt], y: Rational[BigInt]): Rational[BigInt] = Rational(x.d * y.d, x.q * y.q)(n).simplify
+    override def times(x: Rational[BigInt], y: Rational[BigInt]): Rational[BigInt] = Rational(x.d * y.d, x.q * y.q).simplify
 
     override def div(x: Rational[BigInt], y: Rational[BigInt]): Rational[BigInt] = {
       if (y.d == 0) throw new ArithmeticException("/ by zero")
-      else Rational(x.d * y.q, x.q * y.d)(n).simplify
+      else Rational(x.d * y.q, x.q * y.d).simplify
     }
-    override def negate(x: Rational[BigInt]): Rational[BigInt] = Rational(-x.d, x.q)(n).simplify
+    override def negate(x: Rational[BigInt]): Rational[BigInt] = Rational(-x.d, x.q).simplify
 
     override def fromInt(x: Int): Rational[BigInt] = Rational(x, 1)
 
