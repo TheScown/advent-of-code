c5577cf766df974b8d6f3c7b098af97d6d9484bb
Alex Scown <alex@scown.space>
2024-12-27 11:11:46 +0000

    2024 Day 22 Part 2

--- src/main/scala/advent2024/problems/Day22.scala
+++ src/main/scala/advent2024/problems/Day22.scala
@@ -1,7 +1,7 @@
 package space.scown.adventofcode
 package advent2024.problems
 
-import lib.{Files, Problem}
+import lib.{Files, Problem, Timer}
 
 case class Day22(input: Vector[String]) extends Problem {
   private val PRUNING_CONSTANT = 16777216L
@@ -23,27 +23,35 @@ case class Day22(input: Vector[String]) extends Problem {
       (1 to 2000).foldLeft(Vector(initialSecretNumber)) {(acc, _) => acc :+ nextSecretNumber(acc.last)}
     }
 
-    val prices = allSecretNumbers.map(sns => sns.map(_ % 10))
+    val prices = allSecretNumbers.map(sns => sns.map(_ % 10).map(_.toInt))
 
     val deltas = prices.map(priceRow => priceRow.zip(priceRow.tail).map {
       case (x0, x1) => x1 - x0
     })
 
-    val pricesWithDeltas = prices.zip(deltas)
+    val sequenceMaps = deltas.map { deltaRow =>
+      val sliding = deltaRow.sliding(4)
 
-    val allSequences = for {
-      i <- -9 to 9
-      j <- -9  to 9
-      k <- -9 to 9
-      l <- -9 to 9
-    } yield Seq(i, j, k, l)
+      sliding.zipWithIndex.foldLeft(Map[Vector[Int], Int]()) { case (map, (sequence, index)) =>
+        if (map.contains(sequence)) map
+        else map + (sequence -> index)
+      }
+    }
 
-    val result = allSequences.map { sequence =>
-      pricesWithDeltas.map { case (priceRow, deltaRow) =>
-        val indexOfSequence = deltaRow.sliding(4).indexOf(sequence)
+    val pricesWithSequenceIndexes = prices.zip(sequenceMaps)
 
-        if (indexOfSequence == -1) 0
-        else priceRow(indexOfSequence + 4)
+    val allSequences = deltas.flatMap(_.sliding(4))
+
+    val sequenceCounts = allSequences.groupBy(identity).map { case (k, v) => (k, v.size) }
+
+    val sortedSequenceCounts = sequenceCounts.toVector.sortBy(_._2).reverse
+
+    val result = sortedSequenceCounts.map(_._1).take(sortedSequenceCounts.size / 100).map { sequence =>
+      pricesWithSequenceIndexes.map { case (priceRow, sequenceIndex) =>
+        sequenceIndex.get(sequence) match {
+          case Some(i) => priceRow(i + 4)
+          case None => 0
+        }
       }.sum
     }.max
 
@@ -62,5 +70,5 @@ case object Day22 extends App {
   val input = Files.lines("2024/day22.txt")
   val problem = Day22(input)
   problem.solve1()
-  problem.solve2()
+  Timer.time(() => problem.solve2())
 }
