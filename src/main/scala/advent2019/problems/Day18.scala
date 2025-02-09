16159085babce657b4f321419a2d140268190257
Alex Scown <alex@scown.space>
2025-02-09 10:28:51 +0000

    2019 Day 18 (faster)

--- src/main/scala/advent2019/problems/Day18.scala
+++ src/main/scala/advent2019/problems/Day18.scala
@@ -1,6 +1,7 @@
 package space.scown.adventofcode
 package advent2019.problems
 
+import lib.BFS.PathState
 import lib._
 
 case class Day18(input: Vector[String]) extends Problem {
@@ -10,28 +11,25 @@ case class Day18(input: Vector[String]) extends Problem {
     val initialState = State(start, Set(), 0)
 
     val keySet = ('a' to 'z').toSet
-    val keyCount = grid.count(keySet.contains)
+    val keyPositions = grid.indices.filter(p => keySet.contains(grid(p))).toSet
+    val keyCount = keyPositions.size
     val doorSet = ('A' to 'Z').toSet
+
+    val dependencyGraph = buildDependencies(Vector(start), keyPositions, grid, doorSet)
 
     val resultState = Dijkstra.solve[State](
       initialState,
       Ordering.by[State, Int](_.steps).reverse,
       state => state.keys.size == keyCount
     ) { state =>
-      BFS.reachable(state) { bfsState =>
-        val current = grid(bfsState.position)
+      val position = state.position
+      val newDestinations = dependencyGraph(position)
 
-        if (keySet.contains(current) && !state.keys.contains(current) || doorSet.contains(current) && !bfsState.keys.contains(current.toLower)) Seq()
-        else {
-          val neighbours = grid.neighbours(bfsState.position).filter(n => grid(n) != '#')
-
-          neighbours.map { n =>
-            val atN = grid(n)
-            val newKeys = if (keySet.contains(atN)) bfsState.keys + atN else bfsState.keys
-            State(n, newKeys, bfsState.steps + 1)
-          }
-        }
-      }.map(_.value).filter(_.keys.size == state.keys.size + 1).toSeq
+      newDestinations.flatMap {
+        case PathState(DependencyState(destination, dependencies), steps) =>
+          if (!dependencies.forall(state.keys.contains)) Seq()
+          else Seq(State(destination, state.keys + grid(destination), state.steps + steps))
+      }.filter(_.keys.size == state.keys.size + 1)
     }
 
     val result = resultState.get.steps
@@ -48,10 +46,13 @@ case class Day18(input: Vector[String]) extends Problem {
     val updatedGrid = diagonals.foldLeft(gridWithWalls)((grid, d) => grid.updated(d, '@'))
 
     val keySet = ('a' to 'z').toSet
-    val keyCount = updatedGrid.count(keySet.contains)
+    val keyPositions = grid.indices.filter(p => keySet.contains(grid(p))).toSet
+    val keyCount = keyPositions.size
     val doorSet = ('A' to 'Z').toSet
 
     val starts = updatedGrid.zipWithIndex.filter(_._1 == '@').map(_._2).toVector
+    val dependencyGraph = buildDependencies(starts, keyPositions, updatedGrid, doorSet)
+
     val initialState = MultiState(starts, Set(), 0)
 
     val resultState = Dijkstra.solve[MultiState](
@@ -60,28 +61,45 @@ case class Day18(input: Vector[String]) extends Problem {
       state => state.keys.size == keyCount
     ) { state =>
       state.positions.indices.flatMap { i =>
-        val set = BFS.reachable(state) { bfsState =>
-          val position = bfsState.positions(i)
-          val current = updatedGrid(position)
+        val position = state.positions(i)
+        val newDestinations = dependencyGraph(position)
 
-          if (keySet.contains(current) && !state.keys.contains(current) || doorSet.contains(current) && !bfsState.keys.contains(current.toLower)) Seq()
-          else {
-            val neighbours = updatedGrid.neighbours(position).filter(n => updatedGrid(n) != '#')
-
-            neighbours.map { n =>
-              val atN = updatedGrid(n)
-              val newKeys = if (keySet.contains(atN)) bfsState.keys + atN else bfsState.keys
-              MultiState(bfsState.positions.updated(i, n), newKeys, bfsState.steps + 1)
-            }
-          }
+        newDestinations.flatMap {
+          case PathState(DependencyState(destination, dependencies), steps) =>
+            if (!dependencies.forall(state.keys.contains)) Seq()
+            else Seq(MultiState(state.positions.updated(i, destination), state.keys + grid(destination), state.steps + steps))
         }
-        set
-      }.map(_.value).filter(_.keys.size == state.keys.size + 1)
+      }.filter(_.keys.size == state.keys.size + 1)
     }
 
     val result = resultState.get.steps
 
     println(s"Result 2: $result")
+  }
+
+  private def buildDependencies(
+    startPositions: Vector[Complex[Int]],
+    keyPositions: Set[Complex[Int]],
+    grid: Grid[Char],
+    doorSet: Set[Char]
+  ): Map[Complex[Int], Vector[PathState[DependencyState]]] = {
+    (keyPositions ++ startPositions).map { initialPosition
+    =>
+      val initialState = DependencyState(initialPosition, Set())
+
+      val reachableStates = BFS.reachable[DependencyState](initialState) { state =>
+        val current = grid(state.position)
+        val neighbours = grid.neighbours(state.position).filter(n => grid(n) != '#')
+
+        val newDependencies = if (doorSet.contains(current)) state.dependencies + current.toLower else state.dependencies
+
+        neighbours.map(n => DependencyState(n, newDependencies))
+      }
+
+      val validStates = reachableStates.filter(s => keyPositions.contains(s.value.position))
+
+      initialPosition -> validStates.toVector
+    }.toMap
   }
 
   private case class State(position: Complex[Int], keys: Set[Char], steps: Int) {
@@ -98,6 +116,15 @@ case class Day18(input: Vector[String]) extends Problem {
 
     override def equals(obj: Any): Boolean = obj match {
       case MultiState(otherPositions, otherKeys, _) => positions == otherPositions && keys == otherKeys
+      case _ => false
+    }
+  }
+
+  private case class DependencyState(position: Complex[Int], dependencies: Set[Char]) {
+    override def hashCode(): Int = position.hashCode()
+
+    override def equals(obj: Any): Boolean = obj match {
+      case DependencyState(otherDestination, _) => position == otherDestination
       case _ => false
     }
   }
